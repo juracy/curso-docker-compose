96617bbc6156654126be40eacced562496c0c73e
Juracy Filho <juracy.filho@dataprev.gov.br>
2017-05-16 18:48:43 -0300

    Versão inicial do frontend

--- run.sh
+++ run.sh
@@ -1,8 +1,2 @@
 docker-compose up -d
-docker-compose ps
-
-echo "Aguardando a carga do postgres"
-sleep 5
-
-docker-compose exec db psql -U postgres -f /scripts/check.sql
-docker-compose down
+docker-compose logs -f -t

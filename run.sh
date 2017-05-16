9c46e440478d5c7160a9afc5172667fce41cbe19
Juracy Filho <juracy.filho@dataprev.gov.br>
2017-05-16 17:59:57 -0300

    Uso de volumes e script de banco de dados

--- run.sh
+++ run.sh
@@ -4,5 +4,5 @@ docker-compose ps
 echo "Aguardando a carga do postgres"
 sleep 5
 
-docker-compose exec db psql -U postgres -c '\l'
+docker-compose exec db psql -U postgres -f /scripts/check.sql
 docker-compose down
